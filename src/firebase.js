c0f00288a5ea20aa8a004cc5147659ae77de627b
hwr12138 <61023981+hwr12138@users.noreply.github.com>
2026-03-19 22:36:16 -0400

    small change

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDU1WSFtQvsQexvOb1dZvvg2bExyQ5QNkc",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "build-a-habit-11b91.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "build-a-habit-11b91",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "build-a-habit-11b91.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "49740107413",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:49740107413:web:0b024b7532c1134477559c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-33BZGJ3HHY"
 };
 
